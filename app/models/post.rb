ba86aea3940ceabfad4a16900bcde6b828af6a44
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-01 09:30:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validate :is_clickbait?
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
 end
